3b069a37ee81afb8812b6d7e1b57ffa08b5601fc
Tom Holmes <tom.holmes@remindapp.org>
2022-09-07 08:13:02 +0100

    Initial Commit

---
+++ Technet iOS Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Technet iOS Task
+//
+//  Created by Tom Holmes on 07/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
