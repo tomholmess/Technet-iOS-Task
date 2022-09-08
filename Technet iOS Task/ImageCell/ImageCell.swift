76efe1507979a7a49a0b7367f6e1f22ae880b878
Tom Holmes <tom.holmes@remindapp.org>
2022-09-08 11:53:40 +0100

    duration wrapper top left corner rounded for aesthetic

--- Technet iOS Task/ImageCell/ImageCell.swift
+++ Technet iOS Task/ImageCell/ImageCell.swift
@@ -10,12 +10,16 @@ import UIKit
 class ImageCell: UICollectionViewCell {
 
     @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var durationWrapper: UIView!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        durationWrapper.layer.cornerRadius = 7
+        durationWrapper.layer.maskedCorners = [.layerMinXMinYCorner]
     }
 
 }
